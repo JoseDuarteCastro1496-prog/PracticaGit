64b93db87d5da76c65fbe9a51ffbc84ce0dcafa7
JoseDuarteCastro <jose.duartecastro@ucr.ac.cr>
2025-07-27 12:25:24 -0600

    Se agrega funcion 3

--- prueba.py
+++ prueba.py
@@ -52,13 +52,12 @@ def sonar (objeto):
 perro1 = perro ();
 sonar (perro1)
 
-
-
 def nueva_funcion ():
     print("En git")
     
 def otra_funcion ():
     print ("funcion 2")
     
-    
+def funcion_3():
+    print ("funcion 3")
     

8285b839a44486c2275471d288fc0e6f1a01ebd1
JoseDuarteCastro <jose.duartecastro@ucr.ac.cr>
2025-07-30 14:49:30 -0600

    Se agrega las nuevas carpetas

--- Clases abstractas/abstratas.py
+++ Clases abstractas/abstratas.py
@@ -1,1 +1,22 @@
- 
+from abc import ABC, abstractmethod 
+
+class Persona (ABC):
+     def __init__(self, nombre, edad ):
+          self.nombre = nombre
+          self.edad = edad
+
+     @abstractmethod 
+     def actividad_realizada(self):
+          pass
+      
+class Estudiante(Persona):
+    def __init__(self, nombre, edad,actividad):
+         super().__init__(nombre, edad)
+         self.actividad = actividad
+
+    def actividad_realizada(self):
+         return self.actividad
+
+estudiante = Estudiante("jose", 28, "Estudiar")
+actividades = estudiante.actividad_realizada();
+print(actividades);
